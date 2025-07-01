c7daec08ebda744a21eb23e2e26cb2a6e8634c1d
Dilexi <dilexilex97@gmail.com>
2025-07-01 18:19:07 +0300

    Сделал все кавычки одинаковыми

--- render_website.py
+++ render_website.py
@@ -10,15 +10,15 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 def rebuild():
     load_dotenv()
     meta_data = os.getenv("META_DATA", "meta_data.json")
-    os.makedirs('./pages', exist_ok=True)
+    os.makedirs("./pages", exist_ok=True)
     with open(meta_data, "r", encoding="utf8") as my_file:
         books = json.load(my_file)
 
     env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"])
     )
-    template = env.get_template('template.html')
+    template = env.get_template("template.html")
 
     books_pages = list(chunked(books, 10))
     for number, book_page in enumerate(books_pages):
@@ -28,16 +28,16 @@ def rebuild():
             current_page = number + 1
         )
 
-        with open(f'./pages/index-{number+1}.html', 'w', encoding="utf8") as file:
+        with open(f"./pages/index-{number+1}.html", "w", encoding="utf8") as file:
             file.write(rendered_page)
 
 
 def main():
     rebuild()
     server = Server()
-    server.watch('template.html', rebuild)
-    server.serve(root='.', default_filename='./pages/index-1.html')
+    server.watch("template.html", rebuild)
+    server.serve(root=".", default_filename="./pages/index-1.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
